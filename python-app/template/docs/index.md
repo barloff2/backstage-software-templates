3035214f6996aabdfec8c7fa1ba60eae46d06e7b
Memesh <memesh@MacBook-Air-de-Memesh.local>
2026-04-15 21:14:15 -0500

    Adds python app software template

---
+++ python-app/template/docs/index.md
@@ -0,0 +1,12 @@
+# Documents for ${{values.app_name}}
+
+This application has two endpoints:
+
+ - `${{values.app_name}}-${{values.app_env}}.test.com/api/v1info`
+ - `${{values.app_name}}-${{values.app_env}}.test.com/api/healthz`
+
+Here you could expand on what each of these endpoinst do.
+
+# How to access the app?
+
+You can access to app by accessing this URL: `${{values.app_name}}-${{values.app_env}}.test.com/api/healthz`
---
+++ python-app/template/src/app.py
@@ -0,0 +1,30 @@
+#'/api/v1/details'
+#'/api/v1/healthz'
+
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/info')
+
+def info():
+    return jsonify({
+        "time": datetime.date.today().strftime("%B %d, %Y"),
+        "hostname": socket.gethostname(),
+        "message": "ahora si me veo!",
+        'deploy_on': 'kubernetes',
+        'env': '${{values.app_env}}',
+        'app_name': '${{values.app_name}}'
+    })
+
+
+@app.route('/api/v1/healthz')
+
+def healthz():
+    return jsonify({"status": "up"}), 200
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
